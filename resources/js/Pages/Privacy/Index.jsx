a647f9243fdafbcb20b792c6e17553eca959ff2d
titan <titanio.yudista@cakrawala.ac.id>
2025-06-30 18:14:14 +0700

    fix: button back

--- resources/js/Pages/Privacy/Index.jsx
+++ resources/js/Pages/Privacy/Index.jsx
@@ -61,7 +61,7 @@ export default function PrivacyPolicy(props) {
                                 href="/"
                                 className="inline-flex items-center gap-x-2 whitespace-nowrap rounded-md bg-emerald-500 px-4 py-2 text-sm font-semibold text-white shadow-sm transition hover:bg-emerald-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-500"
                             >
-                                <ArrowLeftIcon className="h-4 w-4" />
+                                <ArrowLeftIcon className="h-6 w-6" />
                                 Kembali
                             </Link>
                         </div>
