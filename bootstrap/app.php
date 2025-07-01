5f9700be65df88dde8649deeda29d4874c6960e6
titan <titanio.yudista@cakrawala.ac.id>
2025-07-01 20:59:41 +0700

    fix: middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*'); // <-- TAMBAHKAN BARIS INI
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
